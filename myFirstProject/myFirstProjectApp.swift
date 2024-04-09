0b8c9352be106050f126d970d4eb25dfb0a3ea3c
Oli Kelly <93444456+olikelly00@users.noreply.github.com>
2024-04-09 11:40:58 +0100

    Initial Commit

---
+++ myFirstProject/myFirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  myFirstProjectApp.swift
+//  myFirstProject
+//
+//  Created by Oli Kelly on 09/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct myFirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
